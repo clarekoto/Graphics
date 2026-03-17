bd541877c7fdb35e6ba027d531398e528b77563c
Clare <clarejohn105@gmail.com>
2026-03-17 19:41:56 +1000

    implemented the use of new shader class in main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <cstdlib>
+#include "shader.h"
 
 float squareVertices[] = {
      0.5f,  0.5f, 0.0f,  // top right
@@ -21,30 +22,10 @@ unsigned int indices[] = {  // note that we start from 0!
     1, 2, 3    // second triangle
 };
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 uniColor;\n"
-    //"out vec4 vertexColor;\n" // vertex shader to fragment shader
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos,1.0);\n"
-    "   uniColor = aColor;\n"
-    //"   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n" //changes the colour
-    "}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 uniColor;\n"
-    //"uniform vec4 uniColor;\n"
-    "void main()\n"
-    "{\n"
-    "FragColor = vec4(uniColor, 1.0);\n"
-    "} \n";
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -61,6 +42,9 @@ int main() {
     // glViewport(0, 0, 640, 800); don't need since framebuffer_size_callback does it
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    
+    Shader shader("vertex.glsl", "fragment.glsl");
+
     // Element buffer object
     unsigned int EBO;
     glGenBuffers(1, &EBO);
@@ -74,31 +58,6 @@ int main() {
     //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triVertices), triVertices, GL_STATIC_DRAW);
 
-    // Vertex Shader Code
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    
-    // Fragment Shader Code
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    unsigned int shaderProgram = glCreateProgram();
-
-    // Shader Program
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    // Program object that can be activated by calling glUseProgram
-    glUseProgram(shaderProgram);
-
-    // No longer need shader objects since we've linked them to program object
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);  
-
-    // how OpenGL should interpret vertex data
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -107,17 +66,16 @@ int main() {
     glEnableVertexAttribArray(1);
 
     while (!glfwWindowShouldClose(window)) {
-        
+        // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(shaderProgram);
-        float time = glfwGetTime();
-        float greenValue = (sin(time) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "uniColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+       
+        shader.use();
+        // float time = glfwGetTime();
+        // float greenValue = (sin(time) / 2.0f) + 0.5f;
+        // int vertexColorLocation = glGetUniformLocation(shaderProgram, "uniColor");
+        // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
-        
-    
         
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -126,6 +84,8 @@ int main() {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;

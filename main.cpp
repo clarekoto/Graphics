7335c225efff0e60feab86d5e132b39281c46e9a
Clare <clarejohn105@gmail.com>
2026-03-19 15:36:51 +1000

    multiple cubes rotating, so awesome

--- main.cpp
+++ main.cpp
@@ -8,21 +8,53 @@
 #include "shader.h"
 
 float squareVertices[] = {
-     0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left 
-}; 
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.1f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,1.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,1.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f
+};
 
 float triVertices[] = {
     0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 
     -0.5f,-0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
     0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f
-};
-
-unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 3,   // first triangle
-    1, 2, 3    // second triangle
 };
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -34,6 +66,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     GLFWwindow* window = glfwCreateWindow(640, 480, "Project1", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create window" << std::endl;
@@ -41,11 +74,12 @@ int main() {
         return -1;
     }
     glfwMakeContextCurrent(window);
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
     // glViewport(0, 0, 640, 800); don't need since framebuffer_size_callback does it
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    
     Shader shader("vertex.glsl", "fragment.glsl");
 
     // Element buffer object
@@ -58,8 +92,8 @@ int main() {
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);  
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triVertices), triVertices, GL_STATIC_DRAW);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(squareVertices), squareVertices, GL_STATIC_DRAW);
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -72,73 +106,81 @@ int main() {
     // transform = glm::rotate(transform, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
     // transform = glm::scale(transform, glm::vec3(0.5, 0.5, 0.5));
 
-    glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
+//     glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, 0.1f, 100.0f);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); 
+//     glm::mat4 model = glm::mat4(1.0f);
+//     model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f)); 
 
-    glm::mat4 view = glm::mat4(1.0f);
-// note that we're translating the scene in the reverse direction of where we want to move
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); 
+//     glm::mat4 view = glm::mat4(1.0f);
+// // note that we're translating the scene in the reverse direction of where we want to move
+//     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); 
 
-    glm::mat4 projection;
-    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+//     glm::mat4 projection;
+//     projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)  
+};
 
 
+
+    glEnable(GL_DEPTH_TEST); 
     shader.use();
-    
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)640 / (float)480, 0.1f, 100.0f);
+shader.setMat4("projection", projection);
     while (!glfwWindowShouldClose(window)) {
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        shader.use();
+
         glm::mat4 trans = glm::mat4(1.0f);
         trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
         trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-
         
-        
-        unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        //glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         glm::mat4 view          = glm::mat4(1.0f);
-        glm::mat4 projection    = glm::mat4(1.0f);
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        // glm::mat4 projection    = glm::mat4(1.0f);
+     //   model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
         view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
+        // projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
         unsigned int viewLoc  = glGetUniformLocation(shader.ID, "view");
-        // pass them to the shaders (3 different ways)
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
-        shader.setMat4("projection", projection);
+        shader.setMat4("view", view);
+        
+        glBindVertexArray(VAO);
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * (i+1); 
+            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            shader.setMat4("model", model);
 
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         // second triangle
         trans = glm::mat4(1.0f); // reset it to identity matrix
         trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
         float scaleAmount = static_cast<float>(sin(glfwGetTime()));
         trans = glm::scale(trans, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &trans[0][0]);
-
-
-        // float time = glfwGetTime();
-        // float greenValue = (sin(time) / 2.0f) + 0.5f;
-        // int vertexColorLocation = glGetUniformLocation(shaderProgram, "uniColor");
-        // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
-        
-        
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
